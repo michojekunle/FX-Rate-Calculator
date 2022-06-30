862197cf2e4f1def4bbc241ea283d5e9ca896c7d
AMD <michojekunle1@gmail.com>
2022-06-30 10:00:55 -0700

    styling and working on js file

--- main.js
+++ main.js
@@ -1,2 +1,23 @@
-const array = [0, 1, 2];
-console.log(array);
+const currencyEl_one = document.getElementById('currency-one');
+const currencyEl_two = document.getElementById('currency-two');
+const amountEl_one = document.getElementById('amount-one');
+const amountEl_two = document.getElementById('amount-two');
+const rateEl = document.getElementById('rate');
+const swap = document.getElementById('swap');
+
+//fetching exchange rate and updating to DOM
+
+const calculate = () => {
+    const currency_one = currencyEl_one.value;
+    const currency_two = currencyEl_two.value;
+
+    window.fetch(` https://v6.exchangerate-api.com/v6/72bdc46d161e135aa5232eda/latest/${currency_one}`)
+    .then(res => res.json())
+    .then(data => {
+        const rate = data.conversion_rates[currency_two];
+
+        rateEl.innerText = `1${currency_one} = ${rate} ${currency_two}`
+    })
+}
+
+calculate();
